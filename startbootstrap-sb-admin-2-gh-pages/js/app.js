0cdecb0ca1d749f5688ae3f25f3280c36f43cdbe
Alien <kendgibsonjr@gmail.com>
2021-04-25 14:09:03 -0500

    added second form edit/show button

--- startbootstrap-sb-admin-2-gh-pages/js/app.js
+++ startbootstrap-sb-admin-2-gh-pages/js/app.js
@@ -1,14 +1,18 @@
 
-// getting the input type
-const ssn = document.getElementById('inputSSN')
+// getting the input
 
-console.log(ssn)
+const ssn = document.getElementById('inputSSN')
+const ssn2 = document.getElementById('inputSSN2')
+
 
 // getting the edit button
 
 const edit = document.getElementById('editNShowBtn')
-
+const edit2 = document.getElementById('editNShowBtn2')
 edit.addEventListener('click' , showCon)
+edit2.addEventListener('click' , showCon2)
+
+
 
 // showCon function
 
@@ -24,5 +28,19 @@ function showCon(){
     }
     console.log(ssn.type)
     return ssn.type
+    
+}
+function showCon2(){
+    switch(ssn2.type){
+        case ssn2.type = 'password':
+            ssn2.type = 'text'
+        break;
+
+        case ssn2.type = 'text':
+            ssn2.type = 'password'
+        break;
+    }
+    console.log(ssn2.type)
+    return ssn2.type
     
 }
